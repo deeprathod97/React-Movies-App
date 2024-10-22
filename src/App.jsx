d91d21636de36f8af559dc17c2da8d09beea3f1e
deeprathod97 <deeprathod9797@gmail.com>
2024-10-22 19:38:18 +0530

    react-Movies-app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import Movie from "./components/Movie.jsx";
+import SearchBar from "./components/SearchBar.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar></SearchBar>
+    </div>
+  );
+}
+
+export default App;
